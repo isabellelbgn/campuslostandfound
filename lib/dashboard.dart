6b8616f60083d6246a951eeff7a7847d51060c30
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2024-11-15 21:11:11 +0800

    refactor: separate components into different files

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,7 +1,11 @@
-import 'package:campuslostandfound/auth_screen.dart';
+import 'package:campuslostandfound/auth_page.dart';
+import 'package:campuslostandfound/see_all_items_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import './components/search_bar.dart';
+import './components/category_filter.dart';
+import './components/item_container.dart';
 import 'item_detail_page.dart';
 
 class Dashboard extends StatefulWidget {
@@ -15,12 +19,26 @@ class _DashboardState extends State<Dashboard> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController _searchController = TextEditingController();
   String _searchQuery = "";
+  String _selectedCategory = "All";
 
-  Future<List<Map<String, dynamic>>> fetchItems() async {
+  Future<List<Map<String, dynamic>>> fetchItems(
+      {bool showTodayOnly = true}) async {
     List<Map<String, dynamic>> itemsList = [];
+    DateTime now = DateTime.now();
+    DateTime startOfDay = DateTime(now.year, now.month, now.day);
 
     try {
-      QuerySnapshot querySnapshot = await _firestore.collection("items").get();
+      QuerySnapshot querySnapshot;
+      if (showTodayOnly) {
+        querySnapshot = await _firestore
+            .collection("items")
+            .where("time", isGreaterThanOrEqualTo: startOfDay)
+            .where("time", isLessThan: startOfDay.add(const Duration(days: 1)))
+            .get();
+      } else {
+        querySnapshot = await _firestore.collection("items").get();
+      }
+
       for (var docSnapshot in querySnapshot.docs) {
         final itemData = docSnapshot.data() as Map<String, dynamic>;
         itemData['id'] = docSnapshot.id;
@@ -32,20 +50,32 @@ class _DashboardState extends State<Dashboard> {
     return itemsList;
   }
 
-  // Filter items
   List<Map<String, dynamic>> _filterItems(List<Map<String, dynamic>> items) {
-    if (_searchQuery.isEmpty) {
-      return items;
+    List<Map<String, dynamic>> filteredItems = items;
+
+    // Filter by category
+    if (_selectedCategory != "All") {
+      filteredItems = filteredItems
+          .where((item) =>
+              (item['category'] ?? '').toLowerCase() ==
+              _selectedCategory.toLowerCase())
+          .toList();
     }
-    return items
-        .where((item) =>
-            (item['name'] ?? '')
-                .toLowerCase()
-                .contains(_searchQuery.toLowerCase()) ||
-            (item['description'] ?? '')
-                .toLowerCase()
-                .contains(_searchQuery.toLowerCase()))
-        .toList();
+
+    // Filter by search query
+    if (_searchQuery.isNotEmpty) {
+      filteredItems = filteredItems
+          .where((item) =>
+              (item['name'] ?? '')
+                  .toLowerCase()
+                  .contains(_searchQuery.toLowerCase()) ||
+              (item['description'] ?? '')
+                  .toLowerCase()
+                  .contains(_searchQuery.toLowerCase()))
+          .toList();
+    }
+
+    return filteredItems;
   }
 
   @override
@@ -56,6 +86,7 @@ class _DashboardState extends State<Dashboard> {
         title: const Text(
           "Dashboard",
           style: TextStyle(color: Colors.black),
+          overflow: TextOverflow.visible,
         ),
         actions: [
           // Sign Out Button
@@ -73,114 +104,103 @@ class _DashboardState extends State<Dashboard> {
             },
           ),
         ],
-
-        // Search Bar
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(56.0),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            SearchItemBar(
               controller: _searchController,
-              decoration: InputDecoration(
-                hintText: "Search items...",
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.grey),
-                ),
-              ),
               onChanged: (query) {
                 setState(() {
                   _searchQuery = query;
                 });
               },
             ),
-          ),
-        ),
-      ),
-      body: FutureBuilder<List<Map<String, dynamic>>>(
-        future: fetchItems(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text("Error: ${snapshot.error}"));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text("No items found."));
-          } else {
-            List<Map<String, dynamic>> items = snapshot.data!;
-            List<Map<String, dynamic>> filteredItems = _filterItems(items);
+            const SizedBox(height: 10),
+            CategoryFilter(
+              selectedCategory: _selectedCategory,
+              onCategorySelected: (category) {
+                setState(() {
+                  _selectedCategory = category;
+                });
+              },
+            ),
+            Expanded(
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: fetchItems(showTodayOnly: true),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text("Error: ${snapshot.error}"));
+                  } else {
+                    List<Map<String, dynamic>> items = snapshot.data ?? [];
+                    List<Map<String, dynamic>> filteredItems =
+                        _filterItems(items);
 
-            return ListView.builder(
-              itemCount: filteredItems.length,
-              itemBuilder: (context, index) {
-                var item = filteredItems[index];
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            ItemDetailPage(itemId: item['id']),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 8.0, horizontal: 16.0),
-                    padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.3),
-                          spreadRadius: 2,
-                          blurRadius: 4,
-                        ),
-                      ],
-                    ),
-                    // Item Container
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    return Column(
                       children: [
-                        item['imageUrl'] != null
-                            ? Image.network(
-                                item['imageUrl'] is List
-                                    ? (item['imageUrl'] as List<dynamic>).first
-                                    : item['imageUrl'],
-                                height: 150,
-                                width: double.infinity,
-                                fit: BoxFit.cover,
-                              )
-                            : const Text('No Image Available'),
-                        const SizedBox(height: 8),
-                        Text(
-                          item['name'] ?? 'No Name',
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                "Lost Items Today",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SeeAllItemsPage(),
+                                    ),
+                                  );
+                                },
+                                style: TextButton.styleFrom(
+                                  foregroundColor: Colors.blue[900],
+                                ),
+                                child: const Text("See All Items"),
+                              ),
+                            ],
                           ),
                         ),
-                        const SizedBox(height: 8),
-                        Text(
-                          item['description'] ?? 'No Description',
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          item['location'] ?? 'No Location',
-                          style:
-                              const TextStyle(fontSize: 14, color: Colors.grey),
+                        Expanded(
+                          child: filteredItems.isEmpty
+                              ? const Center(
+                                  child: Text("No items found today."))
+                              : ListView.builder(
+                                  itemCount: filteredItems.length,
+                                  itemBuilder: (context, index) {
+                                    var item = filteredItems[index];
+                                    return ItemContainer(
+                                      item: item,
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                ItemDetailPage(
+                                                    itemId: item['id']),
+                                          ),
+                                        );
+                                      },
+                                    );
+                                  },
+                                ),
                         ),
                       ],
-                    ),
-                  ),
-                );
-              },
-            );
-          }
-        },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/see_all_items_page.dart
@@ -0,0 +1,116 @@
+import 'package:campuslostandfound/item_detail_page.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import './components/item_container.dart';
+import './components/search_bar.dart';
+
+class SeeAllItemsPage extends StatefulWidget {
+  const SeeAllItemsPage({super.key});
+
+  @override
+  State<SeeAllItemsPage> createState() => _SeeAllItemsPageState();
+}
+
+class _SeeAllItemsPageState extends State<SeeAllItemsPage> {
+  final TextEditingController _searchController = TextEditingController();
+  String _searchQuery = "";
+
+  Future<List<Map<String, dynamic>>> fetchAllItems() async {
+    List<Map<String, dynamic>> itemsList = [];
+
+    try {
+      QuerySnapshot querySnapshot =
+          await FirebaseFirestore.instance.collection("items").get();
+      for (var docSnapshot in querySnapshot.docs) {
+        final itemData = docSnapshot.data() as Map<String, dynamic>;
+        itemData['id'] = docSnapshot.id;
+        itemsList.add(itemData);
+      }
+    } catch (e) {
+      print("Error fetching items: $e");
+    }
+    return itemsList;
+  }
+
+  List<Map<String, dynamic>> _filterItems(List<Map<String, dynamic>> items) {
+    if (_searchQuery.isEmpty) {
+      return items;
+    } else {
+      return items
+          .where((item) =>
+              (item['name'] ?? '')
+                  .toLowerCase()
+                  .contains(_searchQuery.toLowerCase()) ||
+              (item['description'] ?? '')
+                  .toLowerCase()
+                  .contains(_searchQuery.toLowerCase()))
+          .toList();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          "All Items",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            SearchItemBar(
+              controller: _searchController,
+              onChanged: (query) {
+                setState(() {
+                  _searchQuery = query;
+                });
+              },
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: fetchAllItems(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text("Error: ${snapshot.error}"));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text("No items found."));
+                  } else {
+                    List<Map<String, dynamic>> items = snapshot.data!;
+                    List<Map<String, dynamic>> filteredItems =
+                        _filterItems(items);
+
+                    return ListView.builder(
+                      itemCount: filteredItems.length,
+                      itemBuilder: (context, index) {
+                        var item = filteredItems[index];
+                        return ItemContainer(
+                          item: item,
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    ItemDetailPage(itemId: item['id']),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
