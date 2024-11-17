98977762e56c9b491aa19a0384f8b1cac347b3c0
Martina <martina1234angeles@gmail.com>
2024-11-18 00:37:59 +0800

    chore: go back navigation

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,7 +1,9 @@
+import 'package:campuslostandfound/main.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:campuslostandfound/screens/dashboard_screen.dart';
 import 'package:campuslostandfound/services/google_auth_service.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -85,77 +87,106 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 70),
         width: double.infinity,
         height: double.infinity,
         decoration: const BoxDecoration(
           color: Colors.white,
         ),
-        child: Builder(
-          builder: (context) {
-            return Center(
-              child: _user == null
-                  ? Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: _signInAsGuest,
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xFF002EB0),
-                              foregroundColor: Colors.white,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 30,
-                                vertical: 15,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextButton.icon(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage()),
+                );
+              },
+              icon: const Icon(Icons.arrow_back, color: Color(0xFF002EB0)),
+              label: const Text(
+                "Go Back",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color(0xFF002EB0),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Builder(
+                builder: (context) {
+                  return Center(
+                    child: _user == null
+                        ? Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton.icon(
+                                  onPressed: _signInAsGuest,
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xFF002EB0),
+                                    foregroundColor: Colors.white,
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 30,
+                                      vertical: 15,
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(2),
+                                    ),
+                                  ),
+                                  icon: const FaIcon(
+                                    FontAwesomeIcons.solidUser,
+                                    size: 15,
+                                  ),
+                                  label: const Text(
+                                    "Sign in as Guest",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
                               ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(2),
+                              const SizedBox(height: 30),
+                              SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton.icon(
+                                  onPressed: _signInWithGoogle,
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xFF002EB0),
+                                    foregroundColor: Colors.white,
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 30,
+                                      vertical: 15,
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(2),
+                                    ),
+                                  ),
+                                  icon: Image.network(
+                                    'https://cdn-icons-png.freepik.com/256/16509/16509564.png?semt=ais_hybrid',
+                                    height: 25,
+                                    width: 25,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  label: const Text(
+                                    "Sign in with Google",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ),
                               ),
-                            ),
-                            child: const Text(
-                              "Sign in as Guest",
-                              style: TextStyle(
-                                fontSize: 18,
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 30),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton.icon(
-                            onPressed: _signInWithGoogle,
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xFF002EB0),
-                              foregroundColor: Colors.white,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 30,
-                                vertical: 15,
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(2),
-                              ),
-                            ),
-                            icon: Image.network(
-                              'https://cdn-icons-png.freepik.com/256/16509/16509564.png?semt=ais_hybrid',
-                              height: 25,
-                              width: 25,
-                              fit: BoxFit.cover,
-                            ),
-                            label: const Text(
-                              "Sign in with Google",
-                              style: TextStyle(
-                                fontSize: 18,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  : const SizedBox.shrink(),
-            );
-          },
+                            ],
+                          )
+                        : const SizedBox.shrink(),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
