c6d0169e1d34e3b67d2303173a14e2281545419a
Martina <martina1234angeles@gmail.com>
2024-11-22 00:52:15 +0800

    chore: fix styling

--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -4,6 +4,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import '../components/item_container.dart';
 import '../components/search_bar.dart';
+import 'package:blobs/blobs.dart' as blobs;
+
+typedef FirestoreBlob = Blob;
 
 class SeeAllItemsPage extends StatefulWidget {
   const SeeAllItemsPage({super.key});
@@ -51,13 +54,60 @@ class _SeeAllItemsPageState extends State<SeeAllItemsPage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    double blobSize = screenHeight * 0.3;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Text(
-          "All Items",
-          style:
-              TextStyle(color: Color(0xFF002EB0), fontWeight: FontWeight.bold),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70),
+        child: Stack(
+          children: [
+            Positioned(
+              top: -140,
+              left: -60,
+              child: blobs.Blob.fromID(
+                id: ['18-6-103'],
+                size: blobSize,
+                styles: blobs.BlobStyles(
+                  color: const Color(0xFFE0E6F6),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: -160,
+              left: screenWidth * 0.6,
+              child: blobs.Blob.fromID(
+                id: ['18-6-103'],
+                size: blobSize,
+                styles: blobs.BlobStyles(
+                  color: const Color(0xFF002EB0),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'lib/assets/icons/logo.png',
+                height: 100,
+                width: 100,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.grey,
+                  size: 25,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+          ],
         ),
       ),
       body: Padding(
@@ -94,23 +144,29 @@ class _SeeAllItemsPageState extends State<SeeAllItemsPage> {
                     List<Map<String, dynamic>> filteredItems =
                         _filterItems(items);
 
-                    return ListView.builder(
-                      itemCount: filteredItems.length,
-                      itemBuilder: (context, index) {
-                        var item = filteredItems[index];
-                        return ItemContainer(
-                          item: item,
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    ItemDetailPage(itemId: item['id']),
-                              ),
-                            );
-                          },
-                        );
-                      },
+                    return SingleChildScrollView(
+                      child: Wrap(
+                        spacing: 10,
+                        runSpacing: 10,
+                        children: List.generate(filteredItems.length, (index) {
+                          var item = filteredItems[index];
+                          return Container(
+                            width: (MediaQuery.of(context).size.width - 50) / 2,
+                            child: ItemContainer(
+                              item: item,
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        ItemDetailPage(itemId: item['id']),
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                        }),
+                      ),
                     );
                   }
                 },
