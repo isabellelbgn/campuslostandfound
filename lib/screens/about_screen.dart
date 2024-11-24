c3803f32ff239f02d617a0bf3f8c1560e67ef701
Martina <martina1234angeles@gmail.com>
2024-11-25 05:36:21 +0800

    chore: display text and images

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -4,7 +4,7 @@ import 'package:campuslostandfound/components/dashboard_drawer.dart';
 import 'package:blobs/blobs.dart' as blobs;
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AboutScreen extends StatefulWidget {
   const AboutScreen({super.key});
@@ -69,15 +69,172 @@ class _AboutScreenState extends State<AboutScreen> {
                 ),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Text(
-                  " About Us ",
-                  style: const TextStyle(
-                      color: Color(0xFF002EB0), fontWeight: FontWeight.bold),
-                ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+              child: Column(
+                children: [
+                  const Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      " About Us ",
+                      style: const TextStyle(
+                          color: Color(0xFF002EB0),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(height: 60),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Image.asset(
+                          'lib/assets/icons/Labuguen.png',
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      const Align(
+                        alignment: Alignment.centerRight,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Isabelle Labuguen",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF002EB0)),
+                            ),
+                            Text(
+                              "Computer Science 4A",
+                              style:
+                                  TextStyle(color: Colors.grey, fontSize: 10),
+                            ),
+                            SizedBox(height: 20),
+                            SizedBox(
+                              width: 200,
+                              child: Text(
+                                "Description..... Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                style: TextStyle(
+                                  color: Color(0xFF002EB0),
+                                  fontSize: 10,
+                                ),
+                                textAlign: TextAlign.start,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Row(
+                              children: [
+                                FaIcon(
+                                  FontAwesomeIcons.linkedin,
+                                  size: 20,
+                                  color: Colors.grey,
+                                ),
+                                SizedBox(width: 10),
+                                FaIcon(
+                                  FontAwesomeIcons.facebookMessenger,
+                                  size: 20,
+                                  color: Colors.grey,
+                                ),
+                                SizedBox(width: 10),
+                                FaIcon(
+                                  FontAwesomeIcons.github,
+                                  size: 20,
+                                  color: Colors.grey,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 80),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Isabelle Labuguen",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF002EB0)),
+                          ),
+                          Text(
+                            "Computer Science 4A",
+                            style: TextStyle(color: Colors.grey, fontSize: 10),
+                          ),
+                          SizedBox(height: 20),
+                          SizedBox(
+                            width: 200,
+                            child: Text(
+                              "Description..... Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                              style: TextStyle(
+                                color: Color(0xFF002EB0),
+                                fontSize: 10,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              FaIcon(
+                                FontAwesomeIcons.linkedin,
+                                size: 20,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(width: 10),
+                              FaIcon(
+                                FontAwesomeIcons.facebookMessenger,
+                                size: 20,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(width: 10),
+                              FaIcon(
+                                FontAwesomeIcons.github,
+                                size: 20,
+                                color: Colors.grey,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Image.asset(
+                        'lib/assets/icons/Angeles.png',
+                        height: 150,
+                        width: 150,
+                        fit: BoxFit.contain,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 100),
+                  const Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Column(
+                      children: [
+                        const Text(
+                          "The best way to find yourself is to lose yourself in the service of others.",
+                          textAlign: TextAlign.center,
+                          style:
+                              TextStyle(color: Color(0xFF002EB0), fontSize: 12),
+                        ),
+                        const Text(
+                          "- Mahatma Gandhi",
+                          style: TextStyle(
+                              color: Color(0xFF002EB0),
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
